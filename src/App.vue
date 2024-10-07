66e70833f78a65e831ad80288db9267e50156cdf
godzhang <zhangqijilindaxue@126.com>
2024-10-07 16:58:42 +0800

    工作流调整

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>张奇实现的 github actions 自动部署</div>
 </template>
 
 <style scoped>
