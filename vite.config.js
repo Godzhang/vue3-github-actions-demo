1d080dd4ec4cdaace23f1883568fb04de5796cb9
godzhang <zhangqijilindaxue@126.com>
2024-10-08 07:28:05 +0800

    基础路径调整

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue3-github-actions-demo/",
+  base: "/github-actions/",
   plugins: [vue()],
 });
